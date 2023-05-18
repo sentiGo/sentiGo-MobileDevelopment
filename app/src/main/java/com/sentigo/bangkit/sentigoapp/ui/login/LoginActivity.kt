0dd99fd1b0cdcbc5836dadbf4591ee9146ac5c84
Nessa <Nessafitri16@gmail.com>
2023-05-19 00:56:07 +0700

    Menyiapkan activity register dan login

---
+++ app/src/main/java/com/sentigo/bangkit/sentigoapp/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.sentigo.bangkit.sentigoapp.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sentigo.bangkit.sentigoapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
